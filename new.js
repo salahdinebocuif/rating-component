44214e14b1fc817e08e762695709e43a9000f712
salah <salahsalahdine323@gmail.com>
2024-05-08 18:23:40 +0100

    modified file

---
+++ new.js
@@ -0,0 +1,6 @@
+document.getElementById("button").onclick=function(){
+  let div =document.getElementById("item1");
+  let div1=document.getElementById("item2");
+    div.style.display="none";
+    div1.style.display="block";
+}
